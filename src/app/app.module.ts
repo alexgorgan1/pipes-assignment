e91e186a9a7b3abec1656019d2b171fe6cfd4eb7
alexgorgan1 <gorgalex1@gmail.com>
2024-02-23 14:48:08 +0100

    Pipe Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReverseStringPipe } from './reverse-string.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReverseStringPipe,
+    SortPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any): any {
+    const sortedArray = [];
+    value.sort((a, b) => {
+      const nameA = a.name.toUpperCase(); 
+      const nameB = b.name.toUpperCase();
+    
+      if (nameA < nameB) {
+        return -1;
+      } else if (nameA > nameB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return value;
+  }
+
+}
